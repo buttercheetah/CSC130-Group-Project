01cb39afa6af8fc71708ed7ffdc9afb2a80de28c
buttercheetah <buttercheetah@gmail.com>
2022-11-21 19:15:39 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os # * we will definitely need this for moving/getting the files
+
+def GetFilesInDir(dir):
+    #! Alexander Rodin 
+    # * Get a complete list of files in a given directory. For now, ignore directorys
+    pass
+
+def SortFiles(files):
+    #! Noah Liby
+    # * Sort the list of files
+    # I will likely use a dictionary or something.
+    pass
+
+def MoveFiles():
+    #! Noah Liby
+    # * Move the list of files to purposefull location
+    pass
+
+def main():
+    #! Whoever feels like it
+    pass
+
+if __name__ == '__main__': # If script is being run and not imported.
+    main()
