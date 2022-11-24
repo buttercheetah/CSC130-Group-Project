6a9facc019374db357778ec001dfa797ee885c9e
buttercheetah <buttercheetah@gmail.com>
2022-11-24 03:23:22 -0500

    Added Basic User Input/Directory Validation

--- app.py
+++ app.py
@@ -11,14 +11,35 @@ def SortFiles(files):
     # I will likely use a dictionary or something.
     pass
 
-def MoveFiles():
+def MoveFiles(files):
     #! Noah Liby
     # * Move the list of files to purposefull location
     pass
 
+def GetDir():
+    print("Would you like to sort your current directory? [Y/n]")
+    choice = input().lower()
+    if choice == 'n':
+        print("Please enter a directory to sort")
+        DirToSort = input()
+    else:
+        DirToSort = os.getcwd()
+    if not os.path.isdir(DirToSort): # Validate that given directory exists
+        print("Directory does not exist")
+        return False
+    return DirToSort
+
 def main():
     #! Whoever feels like it
-    pass
+
+    #* Noah Liby
+    DirToSort = False
+    while not DirToSort: # While DirToSort is not defined, continue calling the GetDir function
+        DirToSort = GetDir()
+    FilesToSort = GetFilesInDir(DirToSort) # Get the files in the given directory
+    SortedFiles = SortFiles(FilesToSort) # Sort the list of files
+    MoveFiles(SortedFiles) # Move the files
+    print("Done")
 
 if __name__ == '__main__': # If script is being run and not imported.
     main()
