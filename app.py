f88af916e807ac5d40b4b1fc8dc5e0ba9f4769a9
alexrdwaketech <arodin@my.waketech.edu>
2022-11-26 11:06:22 -0500

    chore: format app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-import os # * we will definitely need this for moving/getting the files
+import os  # * we will definitely need this for moving/getting the files
 
-def GetFilesInDir(dir):
-    #! Alexander Rodin 
+
+def get_files(dir):
     # * Get a complete list of files in a given directory. For now, ignore directorys
     pass
 
+
 def SortFiles(files):
-    #! Noah Liby
+    # ! Noah Liby
     # * Sort the list of files
     # I will likely use a dictionary or something.
     pass
 
+
 def MoveFiles(files):
-    #! Noah Liby
+    # ! Noah Liby
     # * Move the list of files to purposefull location
     pass
+
 
 def GetDir():
     print("Would you like to sort your current directory? [Y/n]")
@@ -24,22 +27,24 @@ def GetDir():
         DirToSort = input()
     else:
         DirToSort = os.getcwd()
-    if not os.path.isdir(DirToSort): # Validate that given directory exists
+    if not os.path.isdir(DirToSort):  # Validate that given directory exists
         print("Directory does not exist")
         return False
     return DirToSort
 
-def main():
-    #! Whoever feels like it
 
-    #* Noah Liby
+def main():
+    # ! Whoever feels like it
+
+    # * Noah Liby
     DirToSort = False
-    while not DirToSort: # While DirToSort is not defined, continue calling the GetDir function
+    while not DirToSort:  # While DirToSort is not defined, continue calling the GetDir function
         DirToSort = GetDir()
-    FilesToSort = GetFilesInDir(DirToSort) # Get the files in the given directory
-    SortedFiles = SortFiles(FilesToSort) # Sort the list of files
-    MoveFiles(SortedFiles) # Move the files
+    FilesToSort = get_files(DirToSort)  # Get the files in the given directory
+    SortedFiles = SortFiles(FilesToSort)  # Sort the list of files
+    MoveFiles(SortedFiles)  # Move the files
     print("Done")
 
-if __name__ == '__main__': # If script is being run and not imported.
+
+if __name__ == '__main__':  # If script is being run and not imported.
     main()
