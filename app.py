f2c07a3f5b2c584001faeb9bfa4104136170b647
alexrdwaketech <arodin@my.waketech.edu>
2022-11-27 09:41:57 -0500

    feat: get_file_info, get_files, move_files functions

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 import os  # * we will definitely need this for moving/getting the files
+from os.path import join, getsize, splitext
 
 
-def get_files(dir):
+def get_file_info(file, directory):
+    return {
+        "name": file,
+        "size": getsize(join(directory, file)),
+        "ext": splitext(file)[1],
+        "original_path": join(directory, file),
+        "destination_path": "",
+        "move_required": False,
+    }
+
+
+def get_files(directory):
     # * Get a complete list of files in a given directory. For now, ignore directorys
-    pass
+    files_dict = {}
+
+    for root, dirs, files in os.walk(directory):
+        files_dict[root] = [get_file_info(item, root) for item in files]
+
+    return files_dict
 
 
-def SortFiles(files):
+def sort_files(files):
     # ! Noah Liby
     # * Sort the list of files
     # I will likely use a dictionary or something.
-    pass
+    # File will be moved to its destination if move_required is set to True
+    # Do not forget to set destination path
+    return files
 
 
-def MoveFiles(files):
-    # ! Noah Liby
-    # * Move the list of files to purposefull location
-    pass
+def move_files(files):
+    for item in files.items():
+        for file in item[1]:
+            if file['move_required'] and file['destination'] != '':
+                os.rename(file['source'], file['destination'])
 
 
 def GetDir():
@@ -35,15 +55,18 @@ def GetDir():
 
 def main():
     # ! Whoever feels like it
+    # Just example
+    files = get_files('/Users/alex/Downloads')
+    move_files(sort_files(files))
 
     # * Noah Liby
-    DirToSort = False
-    while not DirToSort:  # While DirToSort is not defined, continue calling the GetDir function
-        DirToSort = GetDir()
-    FilesToSort = get_files(DirToSort)  # Get the files in the given directory
-    SortedFiles = SortFiles(FilesToSort)  # Sort the list of files
-    MoveFiles(SortedFiles)  # Move the files
-    print("Done")
+    # DirToSort = False
+    # while not DirToSort:  # While DirToSort is not defined, continue calling the GetDir function
+    #     DirToSort = GetDir()
+    # FilesToSort = get_files(DirToSort)  # Get the files in the given directory
+    # SortedFiles = SortFiles(FilesToSort)  # Sort the list of files
+    # MoveFiles(SortedFiles)  # Move the files
+    # print("Done")
 
 
 if __name__ == '__main__':  # If script is being run and not imported.
